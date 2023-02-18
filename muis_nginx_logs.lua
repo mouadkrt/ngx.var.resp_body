a86102996a29473a4f5f14c4b46c89daca96c1f1
Sentissi Mouad <m.sentissi@gmail.com>
2023-02-18 23:43:21 +0100

    -

--- muis_nginx_logs.lua
+++ muis_nginx_logs.lua
@@ -9,13 +9,13 @@ http {
 
 	lua_need_request_body on;
 	
-	log_format muisformat 'MUIS6 : [$time_local] (remote_addr:$remote_addr) (remote_user:$remote_user) (request:$request) (status:$status) (body_bytes_sent:$body_bytes_sent) (http_referer:$http_referer) (http_user_agent:$http_user_agent) (http_x_forwarded_for:$http_x_forwarded_for) (request_id:$request_id) (scheme:$scheme) (server_addr:$server_addr) (server_port:$server_port) (uri:$uri) (request_method:$request_method) (query_string:$query_string) (content_length:$content_length) (req_headers:$req_headers) (req_body:$request_body) (resp_body:$resp_body)';
+	log_format muisformat 'MUIS8 : [$time_local] (remote_addr:$remote_addr) (remote_user:$remote_user) (request:$request) (status:$status) (body_bytes_sent:$body_bytes_sent) (http_referer:$http_referer) (http_user_agent:$http_user_agent) (http_x_forwarded_for:$http_x_forwarded_for) (request_id:$request_id) (scheme:$scheme) (server_addr:$server_addr) (server_port:$server_port) (uri:$uri) (request_method:$request_method) (query_string:$query_string) (content_length:$content_length) (req_headers:$req_headers) (req_body:$request_body) (resp_body:$resp_body)';
 			
 server {
 
   listen 8089 default_server;
 
-  lua_need_request_body on;
+  #lua_need_request_body on;
 
   set $resp_body "";
   set $req_headers "";
@@ -24,11 +24,12 @@ server {
   client_max_body_size 16k;
 
   rewrite_by_lua_block {
-            local req_headers = "Headers: ";
+            local req_headers = "Headers: (";
             local h, err = ngx.req.get_headers()
             for k, v in pairs(h) do
-                req_headers = req_headers .. k .. ": " .. v .. "\n";
+                req_headers = req_headers .. "[" .. k .. ": " .. v .. "] ";
             end
+			req_headers = req_headers .. ")";
 
             ngx.var.req_headers = req_headers;
   }
@@ -45,11 +46,11 @@ server {
     location / {
 		default_type text/html;
 		content_by_lua_block {
-			ngx.say("<p>hello, world1</p>")
+			ngx.say("<p>hello, world2</p>")
 		}
 	}
 	
-  access_log  /home/msentissi/fuse-backend/open_resty_lab/logs/req-resp.log muisformat;
+  access_log  logs/req-resp.log muisformat;
 }
 
 }
