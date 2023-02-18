0cb3a5b6c73d5c1a833e690f3cb9825faa33dca7
Sentissi Mouad <m.sentissi@gmail.com>
2023-02-18 17:47:10 +0100

    -

--- muis_nginx_logs.lua
+++ muis_nginx_logs.lua
@@ -9,7 +9,7 @@ http {
 
 	lua_need_request_body on;
 	
-	log_format muisformat 'MUIS6 : [$time_local] (remote_addr:$remote_addr) (remote_user:$remote_user) (request:$request) (status:$status) (body_bytes_sent:$body_bytes_sent) (http_referer:$http_referer) (http_user_agent:$http_user_agent) (http_x_forwarded_for:$http_x_forwarded_for) (request_id:$request_id) (scheme:$scheme) (server_addr:$server_addr) (server_port:$server_port) (uri:$uri) (request_method:$request_method) (query_string:$query_string) (content_length:$content_length) (request_body:$request_body) (resp_body:$resp_body)';
+	log_format muisformat 'MUIS6 : [$time_local] (remote_addr:$remote_addr) (remote_user:$remote_user) (request:$request) (status:$status) (body_bytes_sent:$body_bytes_sent) (http_referer:$http_referer) (http_user_agent:$http_user_agent) (http_x_forwarded_for:$http_x_forwarded_for) (request_id:$request_id) (scheme:$scheme) (server_addr:$server_addr) (server_port:$server_port) (uri:$uri) (request_method:$request_method) (query_string:$query_string) (content_length:$content_length) (req_headers:$req_headers) (req_body:$request_body) (resp_body:$resp_body)';
 			
 server {
 
@@ -18,7 +18,6 @@ server {
   lua_need_request_body on;
 
   set $resp_body "";
-  set $req_body "";
   set $req_headers "";
 
   client_body_buffer_size 16k;
@@ -26,7 +25,6 @@ server {
 
   rewrite_by_lua_block {
             local req_headers = "Headers: ";
-            ngx.var.req_body = ngx.req.get_body_data();
             local h, err = ngx.req.get_headers()
             for k, v in pairs(h) do
                 req_headers = req_headers .. k .. ": " .. v .. "\n";
@@ -47,7 +45,7 @@ server {
     location / {
 		default_type text/html;
 		content_by_lua_block {
-			ngx.say("<p>hello, worldd</p>")
+			ngx.say("<p>hello, world1</p>")
 		}
 	}
 	
